f5c50f80f9594d985f5e4caf4bb1a5c4550b165b
Reyan <reyanzaman.rz@gmail.com>
2026-02-07 13:48:16 +0800

    cv update

--- app/content/artist/page.tsx
+++ app/content/artist/page.tsx
@@ -85,7 +85,7 @@ export default function Artist() {
     {
       title: "Resume",
       icon: <IconFileSpark className="h-full w-full text-neutral-300" />,
-      href: "https://drive.google.com/file/d/1NxtnPEkuVEv-dZUyjdwvC977NYabutWs/view?usp=sharing",
+      href: "https://drive.google.com/file/d/17kC-At15B7KL3qac6hYrfYPIakA2xBNw/view?usp=drive_link",
     },
     {
       title: "Contact",
--- app/content/developer/page.tsx
+++ app/content/developer/page.tsx
@@ -51,7 +51,7 @@ export default function Developer() {
             icon: (
                 <IconFileSpark className="h-full w-full text-neutral-300" />
             ),
-            href: "https://drive.google.com/file/d/1NxtnPEkuVEv-dZUyjdwvC977NYabutWs/view?usp=sharing",
+            href: "https://drive.google.com/file/d/1lgp2eRa9VsYxCxBI8zdqkPa7xtUsJlnO/view?usp=drive_link",
         },
         {
             title: "Contact",
